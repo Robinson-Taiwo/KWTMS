e60c41dad1dc08e6d15989fb146b9a54c4a5b717
Robinson-Taiwo <omoyenisamuell66@gmail.com>
2023-02-27 01:08:05 +0100

    initial commit

---
+++ src/Components/Onboarding.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import "./Onboarding.css"
+
+const Onboarding = () => {
+    return (
+        <div className='container'>
+
+            <div className="picture">
+
+            </div>
+
+            <div className="form-section">
+
+            </div>
+
+        </div>
+    )
+}
+
+export default Onboarding
---
+++ tailwind.config.cjs
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        myGreen: '#3CCC4A',
+        myTeal: "#779341",
+      },
+      fontSize: {
+        '1l': '1rem',
+        'si': '1.23rem',
+        'tf': '1.85rem',
+        'tt': '2.31rem',
+        'st': '1.31rem',
+        'ts': '2rem',
+        'tw': '1.54rem',
+        '1l': '1rem',
+        'ff': '3.46rem',
+      },
+
+    },
+  },
+  plugins: [],
+}
